dc8652ba84e1f2592607b0da7d0b9f5ef7dc66a0
Marcelo Henrique Borgas <106122728+TcheloBorgas@users.noreply.github.com>
2023-12-02 22:24:59 -0300

    teste

---
+++ terraform/aws/main.tf
@@ -0,0 +1,141 @@
+// create two ec2 with load balancer attached to them. The ec2s must have a apache server html page
+
+resource "aws_vpc" "net" {
+  cidr_block = "172.0.0.0/16"
+}
+
+resource "aws_subnet" "net-1" {
+  vpc_id                  = aws_vpc.net.id
+  cidr_block              = "172.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "net-2" {
+  vpc_id                  = aws_vpc.net.id
+  cidr_block              = "172.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+
+resource "aws_internet_gateway" "net" {
+  vpc_id = aws_vpc.net.id
+}
+
+resource "aws_route_table" "net" {
+  vpc_id = aws_vpc.net.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.net.id
+  }
+}
+
+resource "aws_route_table_association" "net-1" {
+  subnet_id      = aws_subnet.net-1.id
+  route_table_id = aws_route_table.net.id
+}
+resource "aws_route_table_association" "net-2" {
+  subnet_id      = aws_subnet.net-2.id
+  route_table_id = aws_route_table.net.id
+}
+
+resource "aws_security_group" "net" {
+  name        = "net"
+  description = "Allow net inbound traffic"
+  vpc_id      = aws_vpc.net.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "net-1" {
+  ami                         = "ami-0230bd60aa48260c6"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.net-1.id
+  vpc_security_group_ids      = [aws_security_group.net.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo '<h1>72 pro exame</h1>' | tee /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "net"
+  }
+}
+
+resource "aws_instance" "net-2" {
+  ami                         = "ami-0230bd60aa48260c6"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.net-1.id
+  vpc_security_group_ids      = [aws_security_group.net.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo '<h1>72 pro exame 2</h1>' | tee /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "net"
+  }
+}
+
+resource "aws_lb" "lb" {
+  name               = "lb-tchelo"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.net-1.id, aws_subnet.net-2.id]
+  security_groups    = [aws_security_group.net.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tg-tchelo"
+  protocol = "HTTP"
+  port     = "80"
+  vpc_id   = aws_vpc.net.id
+}
+
+resource "aws_lb_listener" "ec2_lb_listener" {
+  protocol          = "HTTP"
+  port              = "80"
+  load_balancer_arn = aws_lb.lb.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+resource "aws_lb_target_group_attachment" "net-1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.net-1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "net-2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.net-2.id
+  port             = 80
+}
---
+++ terraform/azure/main.tf
@@ -0,0 +1,73 @@
+resource "azurerm_resource_group" "rg-staticsite" {
+  name     = "poneis"
+  location = "eastus"
+}
+ 
+resource "azurerm_storage_account" "stracctstaticsite" {
+  name                     = var.stracctstaticsite
+  resource_group_name      = azurerm_resource_group.rg-staticsite.name
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "error.html"
+  }
+}
+ 
+resource "azurerm_storage_blob" "index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.stracctstaticsite.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "../app/index.html"
+}
+
+resource "azurerm_storage_blob" "error" {
+  name                   = "error.html"
+  storage_account_name   = azurerm_storage_account.stracctstaticsite.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "../app/error.html"
+}
+
+# ---------------------------------------------
+
+# recurso de ip
+resource "azurerm_public_ip" "lb-pip" {
+  name                = "loadbalancer-public-ip"
+  location            = azurerm_resource_group.rg-staticsite.location
+  resource_group_name = azurerm_resource_group.rg-staticsite.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# recurso load balancer
+
+resource "azurerm_lb" "lb" {
+  name                = "loadbalancer"
+  location            = azurerm_resource_group.rg-staticsite.location
+  resource_group_name = azurerm_resource_group.rg-staticsite.name
+
+  frontend_ip_configuration {
+    name                 = "public-ip-config"
+    public_ip_address_id = azurerm_public_ip.lb-pip.id
+  }
+}
+
+# regras de trafego
+
+resource "azurerm_lb_rule" "lb-rule-http" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "http-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "public-ip-config"
+  enable_floating_ip             = false
+  idle_timeout_in_minutes        = 4
+  load_distribution              = "Default"
+}
